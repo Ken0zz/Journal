91d727bc9dd3f377e7695a0c700c48b34a712986
Ken0zz <72038963+Ken0zz@users.noreply.github.com>
2020-09-30 22:47:30 +0700

    auth

---
+++ Journal/Views/Authorization.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Journal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Journal.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        JournalEntities db = new JournalEntities();
+        public static string nametchr;
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < db.Преподаватель.Count(); i++)
+            {
+                if ((db.Преподаватель.ToList()[i].Логин == login.Text) && (db.Преподаватель.ToList()[i].Пароль == pass.Text))
+                {
+                    nametchr = db.Преподаватель.ToList()[i].Имя + db.Преподаватель.ToList()[i].Отчество;
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Registration reg = new Registration();
+            reg.Show();
+            
+        }
+    }
+}
